a4be67ec9afb02f2f9d019d61783bf94c5162a49
Raquel Sylos <raquelsylos@gmail.com>
2024-11-19 17:02:54 -0300

    rails task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,21 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  get 'up' => 'rails/health#show', as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get 'tasks', to: 'tasks#index'
+  # Create task
+  get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  post 'tasks', to: 'tasks#create'
+  # Find task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # update a task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # Destroy a task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
